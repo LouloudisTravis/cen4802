46aac18c060f76f9aaac0eb54fac7578828f5d1b
Travis Louloudis <travislouloudis@gmail.com>
2020-05-23 22:11:38 -0400

    Init Commit

---
+++ cen4802/src/week01/HelloWorld.java
@@ -0,0 +1,21 @@
+package week01;
+
+/**
+ * 
+ * @author Travis_Louloudis
+ *
+ */
+public class HelloWorld
+{
+	/**
+	 * 
+	 * Hello World + name
+	 * 
+	 * @return String
+	 */
+	public String display()
+	{
+
+		return "Hello World from Travis Louloudis";
+	}
+}
